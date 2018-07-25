337bc7c921a4dab1196e2fb0b4a1664fcf864601
Teylor <teylor6@gmail.com>
2018-07-25 20:13:46 +0200

    Fix Styles

--- back.js
+++ back.js
@@ -1,14 +1,14 @@
 
 
 $(document).ready(function () {
-    $('.js-example-basic-single').select2();
+    $('#heroes-selector').select2();
 
     $.ajax({
         dataType: "json",
         url: "heroes.json",
         success: function (data) {
             $.each(data, function (i, item) {
-                $('#heroes').append($('<option>', {
+                $('#heroes-selector').append($('<option>', {
                     value: item.id,
                     text: item.name
                 }));
